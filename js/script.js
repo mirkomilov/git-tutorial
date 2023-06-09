0cbead7d67382508e1b598e95aaa7011a3cf823b
Doniyorbek <doniyorbekmirkomilov1@gmail.com>
2023-06-09 17:03:01 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const person = {
+  name: "Samar",
+};
+person.name = "Yusuf";
+console.log(person);  
+
+//Integer
+let number = 4.16;
+console.log(8 / 0); //Infinity
+console.log("Samar" * 5);  //NaN
+
+//String
+let text = "Doniyor";
+
+//Boolean
+const isMarried = false;
+const isCircleEarth = true;
+
+//Null
+console.log(clientAge);
+
+//Undefined
+let und;
+console.log(un);
+
+//Object  
+const thief = {
+  age: 30, //integer
+  jacket: "black", //string
+  isLong: false, //boolean
+};
+console.log(thief.age);
+
+//Array
+const colors = ["red", "blue",  "green", 10, [], {}];
+console.log(colors[2]);
+
+
